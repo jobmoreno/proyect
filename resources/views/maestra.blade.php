88b9d04ab4a04922158d630077f8e3480b4232ce
job.moreno@alumnos.udg.mx <inside.9>
2020-05-25 16:01:28 -0500

    proyecto

---
+++ resources/views/maestra.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="{{env("APP_NAME")}}">
+    <title>@yield("titulo") - {{env("APP_NAME")}}</title>
+    <link href="{{url("/css/bootstrap.min.css")}}" rel="stylesheet">
+    <link href="{{url("/css/all.min.css")}}" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 70px;
+            /*Para la barra inferior fija*/
+            padding-bottom: 70px;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <span class="navbar-brand">{{env("APP_NAME")}}</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+            id="botonMenu" aria-label="Mostrar u ocultar menú">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav mr-auto">
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">
+                        Registro
+                    </a>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route("home")}}">Inicio&nbsp;<i class="fa fa-home"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route("productos.index")}}">Productos&nbsp;<i class="fa fa-box"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route("vender.index")}}">Vender&nbsp;<i class="fa fa-cart-plus"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route("ventas.index")}}">Ventas&nbsp;<i class="fa fa-list"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route("usuarios.index")}}">Usuarios&nbsp;<i class="fa fa-users"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route("clientes.index")}}">Clientes&nbsp;<i class="fa fa-users"></i></a>
+                </li>
+            @endguest
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            @auth
+                <li class="nav-item">
+                    <a href="{{route("logout")}}" class="nav-link">
+                        Salir ({{ Auth::user()->nombre }})
+                    </a>
+                </li>
+            @endauth
+        </ul>
+    </div>
+</nav>
+<main class="container-fluid">
+    @yield("contenido")
+</main>
+</body>
+</html>
